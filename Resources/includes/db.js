eb78373aac58b863369f48ddd76311ed53302e5b
outadoc <outadoc@me.com>
2012-06-13 21:32:01 +0200

    Added help text when there is no skin in the app

--- Resources/includes/db.js
+++ Resources/includes/db.js
@@ -59,12 +59,41 @@ function getSkins() {
 	skinList.close();
 
 	if(rows.length == 0) {
-		var row = {
-			title: I('main.noContent'),
+		var row = Ti.UI.createTableViewRow({
 			editable: false,
-			isPlaceholder: true
-		};
-
+			isPlaceholder: true,
+			selectionStyle: Ti.UI.iPhone.TableViewCellSelectionStyle.GRAY
+		});
+		
+		var lbl_title = Ti.UI.createLabel({
+			text: I('main.noContent.title'),
+			font: {
+				fontWeight: 'bold',
+				fontSize: 17
+			},
+			top: 7,
+			left: 10,
+			right: 10,
+			width: Ti.UI.SIZE,
+			height: Ti.UI.SIZE
+		});
+		
+		row.add(lbl_title);
+		
+		var lbl_help = Ti.UI.createLabel({
+			text: I('main.noContent.help'),
+			top: 35,
+			bottom: 10,
+			left: 10,
+			right: 10,
+			font: {
+				fontSize: 15
+			},
+			height: Ti.UI.SIZE,
+			width: Ti.UI.FILL
+		});
+		
+		row.add(lbl_help);
 		rows.push(row);
 	}
 
