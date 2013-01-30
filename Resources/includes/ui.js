fdec79f2bb7bfac330cc4cde6889fc61e99da66d
outadoc <outadoc@gmail.com>
2013-01-30 12:45:37 +0100

    Moved panel-returning function in ui module

--- Resources/includes/ui.js
+++ Resources/includes/ui.js
@@ -1,5 +1,7 @@
 (function() {
 	
+	Ti.include('/includes/lib/json.i18n.js');
+	
 	exports.getEmptyRow = function() {
 		var row = Ti.UI.createTableViewRow({
 			editable: false,
@@ -37,6 +39,138 @@
 		
 		row.add(lbl_help);
 		return row;
+	}
+	
+	exports.getSkinInfoPanel = function(id, description, timestamp) {
+		var infoPanel = Ti.UI.createTableViewRow({
+			height: 200,
+			isInfoPanel: true,
+			editable: false,
+			selectionStyle: Ti.UI.iPhone.TableViewCellSelectionStyle.NONE
+		});
+
+		var panelView = Ti.UI.createView({
+			height: 200,
+			width: 300,
+			backgroundImage: '/img/panel_bg.png'
+		});
+
+		infoPanel.add(panelView);
+
+		var lbl_skin_desc_title = Ti.UI.createLabel({
+			text: I('main.skinDetails.description'),
+			top: 20,
+			left: 135,
+			color: 'darkGray',
+			font: {
+				fontWeight: 'bold'
+			},
+			height: 20,
+			width: 150
+		});
+
+		panelView.add(lbl_skin_desc_title);
+
+		var scrollView_desc = Ti.UI.createScrollView({
+			height: 75,
+			width: 180,
+			top: 45,
+			left: 140,
+			scrollType: 'vertical',
+			showVerticalScrollIndicator: true,
+			contentHeight: 'auto'
+		});
+
+		panelView.add(scrollView_desc);
+
+		var lbl_skin_desc = Ti.UI.createLabel({
+			text: description + '\n ',
+			color: 'darkGray',
+			top: 0,
+			left: 0,
+			font: {
+				fontSize: 13
+			},
+			width: 150,
+			height: Ti.UI.SIZE
+		});
+
+		scrollView_desc.add(lbl_skin_desc);
+
+		var lbl_skin_time_title = Ti.UI.createLabel({
+			text: I('main.skinDetails.creation'),
+			top: 130,
+			left: 135,
+			color: 'darkGray',
+			font: {
+				fontWeight: 'bold'
+			},
+			height: 20,
+			width: 150
+		});
+
+		panelView.add(lbl_skin_time_title);
+
+		var creationDate = new Date(timestamp);
+
+		var lbl_skin_time = Ti.UI.createLabel({
+			text: creationDate.toLocaleDateString(),
+			color: 'darkGray',
+			top: lbl_skin_time_title.getTop() + 25,
+			left: 140,
+			font: {
+				fontSize: 13
+			},
+			height: 'auto',
+			width: 150
+		});
+
+		panelView.add(lbl_skin_time);
+
+		var view_skin = Ti.UI.createImageView({
+			height: 170,
+			width: 85,
+			top: 15,
+			left: 20
+		});
+
+		panelView.add(view_skin);
+
+		var img_skin_front = Ti.UI.createImageView({
+			defaultImage: '/img/char_front.png',
+			image: Ti.Filesystem.getFile(Utils.getSkinsDir() + id + '/front.png').getNativePath(),
+			height: 170,
+			width: 85,
+			top: 0,
+			left: 0
+		});
+
+		view_skin.add(img_skin_front);
+
+		var img_skin_back = Ti.UI.createImageView({
+			defaultImage: '/img/char_back.png',
+			image: Ti.Filesystem.getFile(Utils.getSkinsDir() + id + '/back.png').getNativePath(),
+			height: 170,
+			width: 85,
+			top: 0,
+			left: 0
+		});
+
+		img_skin_front.addEventListener('singletap', function() {
+			view_skin.animate({
+				view: img_skin_back,
+				transition: Ti.UI.iPhone.AnimationStyle.FLIP_FROM_RIGHT
+			});
+		});
+
+		img_skin_back.addEventListener('singletap', function() {
+			view_skin.animate({
+				view: img_skin_front,
+				transition: Ti.UI.iPhone.AnimationStyle.FLIP_FROM_LEFT
+			});
+		});
+		
+		return infoPanel;
 	}
 	
 })();
--- Resources/views_iphone/main.js
+++ Resources/views_iphone/main.js
@@ -2,6 +2,7 @@ Ti.include('/includes/lib/json.i18n.js');
 
 var Database = require('/includes/db');
 var Utils = require('/includes/utils');
+var Ui = require('/includes/ui');
 
 var win = Ti.UI.currentWindow;
 var loadingWin = Utils.createLoadingWindow();
@@ -163,135 +164,7 @@ tableView.addEventListener('delete', function(e) {
 tableView.addEventListener('click', function(e) {
 	if((e.row.children == null || (e.row.children != null && e.source != e.row.children[0])) && e.index != null && !e.rowData.isInfoPanel && !e.rowData.isPlaceholder) {
 		if(!e.rowData.isExpanded) {
-			var infoPanel = Ti.UI.createTableViewRow({
-				height: 200,
-				isInfoPanel: true,
-				editable: false,
-				selectionStyle: Ti.UI.iPhone.TableViewCellSelectionStyle.NONE
-			});
-
-			var panelView = Ti.UI.createView({
-				height: 200,
-				width: 300,
-				backgroundImage: '/img/panel_bg.png'
-			});
-
-			infoPanel.add(panelView);
-
-			var lbl_skin_desc_title = Ti.UI.createLabel({
-				text: I('main.skinDetails.description'),
-				top: 20,
-				left: 135,
-				color: 'darkGray',
-				font: {
-					fontWeight: 'bold'
-				},
-				height: 20,
-				width: 150
-			});
-
-			panelView.add(lbl_skin_desc_title);
-
-			var scrollView_desc = Ti.UI.createScrollView({
-				height: 75,
-				width: 180,
-				top: 45,
-				left: 140,
-				scrollType: 'vertical',
-				showVerticalScrollIndicator: true,
-				contentHeight: 'auto'
-			});
-
-			panelView.add(scrollView_desc);
-
-			var lbl_skin_desc = Ti.UI.createLabel({
-				text: e.rowData.skinData.desc + '\n ',
-				color: 'darkGray',
-				top: 0,
-				left: 0,
-				font: {
-					fontSize: 13
-				},
-				width: 150,
-				height: Ti.UI.SIZE
-			});
-
-			scrollView_desc.add(lbl_skin_desc);
-
-			var lbl_skin_time_title = Ti.UI.createLabel({
-				text: I('main.skinDetails.creation'),
-				top: 130,
-				left: 135,
-				color: 'darkGray',
-				font: {
-					fontWeight: 'bold'
-				},
-				height: 20,
-				width: 150
-			});
-
-			panelView.add(lbl_skin_time_title);
-
-			var creationDate = new Date(e.rowData.skinData.time);
-
-			var lbl_skin_time = Ti.UI.createLabel({
-				text: creationDate.toLocaleDateString(),
-				color: 'darkGray',
-				top: lbl_skin_time_title.getTop() + 25,
-				left: 140,
-				font: {
-					fontSize: 13
-				},
-				height: 'auto',
-				width: 150
-			});
-
-			panelView.add(lbl_skin_time);
-
-			var view_skin = Ti.UI.createImageView({
-				height: 170,
-				width: 85,
-				top: 15,
-				left: 20
-			});
-
-			panelView.add(view_skin);
-
-			var img_skin_front = Ti.UI.createImageView({
-				defaultImage: '/img/char_front.png',
-				image: Ti.Filesystem.getFile(Utils.getSkinsDir() + e.rowData.skinData.id + '/front.png').getNativePath(),
-				height: 170,
-				width: 85,
-				top: 0,
-				left: 0
-			});
-
-			view_skin.add(img_skin_front);
-
-			var img_skin_back = Ti.UI.createImageView({
-				defaultImage: '/img/char_back.png',
-				image: Ti.Filesystem.getFile(Utils.getSkinsDir() + e.rowData.skinData.id + '/back.png').getNativePath(),
-				height: 170,
-				width: 85,
-				top: 0,
-				left: 0
-			});
-
-			img_skin_front.addEventListener('singletap', function() {
-				view_skin.animate({
-					view: img_skin_back,
-					transition: Ti.UI.iPhone.AnimationStyle.FLIP_FROM_RIGHT
-				});
-			});
-
-			img_skin_back.addEventListener('singletap', function() {
-				view_skin.animate({
-					view: img_skin_front,
-					transition: Ti.UI.iPhone.AnimationStyle.FLIP_FROM_LEFT
-				});
-			});
-
-			tableView.insertRowAfter(e.index, infoPanel, {
+			tableView.insertRowAfter(e.index, Ui.getSkinInfoPanel(e.rowData.skinData.id, e.rowData.skinData.desc, e.rowData.skinData.time), {
 				animated: true,
 				animationStyle: Titanium.UI.iPhone.RowAnimationStyle.FADE
 			});
