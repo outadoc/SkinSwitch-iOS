eba620ee7a0d14390295fb491a4cc4dfb9af936e
outadoc <outadoc@gmail.com>
2013-01-30 21:46:16 +0100

    Moved iPad info panel function to ui module

--- Resources/includes/ui.js
+++ Resources/includes/ui.js
@@ -41,7 +41,7 @@
 		return row;
 	}
 	
-	exports.getSkinInfoPanel = function(id, description, timestamp, updateSkinsList) {
+	exports.getiPhoneSkinInfoPanel = function(id, description, timestamp, updateSkinsList) {
 		var infoPanel = Ti.UI.createTableViewRow({
 			height: 200,
 			isInfoPanel: true,
@@ -203,6 +203,176 @@
 		});
 		
 		return infoPanel;
+	}
+	
+	exports.getiPadSkinInfoPanel = function(skinData, win) {
+		var skinInfos = Ti.UI.createView({
+			height: Ti.UI.SIZE,
+			width: Ti.UI.FILL
+		});
+	
+		var lbl_skin_time_title = Ti.UI.createLabel({
+			text: I('main.skinDetails.creation'),
+			top: 20,
+			left: '5%',
+			color: 'darkGray',
+			font: {
+				fontWeight: 'bold',
+				fontSize: 17
+			},
+			height: 20,
+			width: 150
+		});
+	
+		skinInfos.add(lbl_skin_time_title);
+	
+		var creationDate = new Date(skinData.time);
+	
+		var lbl_skin_time = Ti.UI.createLabel({
+			text: creationDate.toLocaleDateString(),
+			color: 'darkGray',
+			top: 40,
+			left: '5%',
+			font: {
+				fontSize: 16
+			},
+			height: 'auto',
+			width: 150
+		});
+	
+		skinInfos.add(lbl_skin_time);
+	
+		var lbl_skin_desc_title = Ti.UI.createLabel({
+			text: I('main.skinDetails.description'),
+			top: 65,
+			left: '5%',
+			color: 'darkGray',
+			font: {
+				fontWeight: 'bold',
+				fontSize: 17
+			},
+			height: 20,
+			width: 150
+		});
+	
+		skinInfos.add(lbl_skin_desc_title);
+	
+		var lbl_skin_desc = Ti.UI.createLabel({
+			text: skinData.desc,
+			color: 'darkGray',
+			width: '50%',
+			height: Ti.UI.SIZE,
+			top: 85,
+			left: '5%',
+			font: {
+				fontSize: 16
+			}
+		});
+	
+		skinInfos.add(lbl_skin_desc);
+	
+		var view_skin = Ti.UI.createImageView({
+			height: 300,
+			width: Ti.UI.SIZE,
+			right: '5%',
+			top: 25
+		});
+	
+		skinInfos.add(view_skin);
+	
+		var img_skin_front = Ti.UI.createImageView({
+			defaultImage: '/img/char_front.png',
+			image: Ti.Filesystem.getFile(Utils.getSkinsDir() + skinData.id + '/front.png').getNativePath(),
+			height: 300,
+			width: 150,
+			top: 0
+		});
+	
+		view_skin.add(img_skin_front);
+	
+		var img_skin_back = Ti.UI.createImageView({
+			defaultImage: '/img/char_back.png',
+			image: Ti.Filesystem.getFile(Utils.getSkinsDir() + skinData.id + '/back.png').getNativePath(),
+			height: 300,
+			width: 150,
+			top: 0
+		});
+	
+		img_skin_front.addEventListener('singletap', function() {
+			view_skin.animate({
+				view: img_skin_back,
+				transition: Ti.UI.iPhone.AnimationStyle.FLIP_FROM_RIGHT
+			});
+		});
+	
+		img_skin_back.addEventListener('singletap', function() {
+			view_skin.animate({
+				view: img_skin_front,
+				transition: Ti.UI.iPhone.AnimationStyle.FLIP_FROM_LEFT
+			});
+		});
+	
+		var b_wear = Ti.UI.createButton({
+			title: I('main.skinDetails.wear'),
+			font: {
+				fontSize: 23
+			},
+			backgroundImage: '/img/button.png',
+			width: Ti.UI.FILL,
+			top: 50,
+			left: '8%',
+			right: '8%',
+			height: 50
+		});
+	
+		b_wear.addEventListener('click', function() {
+			Network.uploadSkin(skinData.id, skinData.name);
+		});
+	
+		var b_close = Ti.UI.createButton({
+			title: I('main.skinDetails.close'),
+			font: {
+				fontSize: 23
+			},
+			backgroundImage: '/img/button.png',
+			width: Ti.UI.FILL,
+			top: 10,
+			left: '8%',
+			right: '8%',
+			height: 50
+		});
+	
+		b_close.addEventListener('click', function() {
+			win.detailWin.remove(win.detailContent);
+			win.detailContent = win.initialInfoView;
+			win.detailContent.setOpacity(0);
+	
+			win.detailWin.add(win.detailContent);
+			win.detailContent.animate({
+				opacity: 1,
+				duration: 300,
+				curve: Ti.UI.iOS.ANIMATION_CURVE_EASE_IN
+			});
+		});
+	
+		win.detailWin.remove(win.detailContent);
+		win.detailContent = Ti.UI.createView({
+			layout: 'vertical',
+			opacity: 0,
+			height: Ti.UI.SIZE,
+			top: '10%'
+		});
+	
+		win.detailContent.add(skinInfos);
+		win.detailContent.add(b_wear);
+		win.detailContent.add(b_close);
+	
+		win.detailWin.add(win.detailContent);
+		win.detailContent.animate({
+			opacity: 1,
+			duration: 300,
+			curve: Ti.UI.iOS.ANIMATION_CURVE_EASE_IN
+		});
 	}
 	
 })();
