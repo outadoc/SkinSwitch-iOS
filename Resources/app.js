ecafcd94a2a00787d1ce0791b11c1434c1a63226
outadoc <outadoc@gmail.com>
2013-03-08 12:33:37 +0100

    Implement backup system for the skins database

--- Resources/app.js
+++ Resources/app.js
@@ -1,172 +1,206 @@
 var Utils = require('/includes/utils');
+var Database = require('/includes/db');
+var db;
+
 Ti.UI.setBackgroundColor('#000');
 
-var db = Ti.Database.open('skins');
-db.execute('CREATE TABLE IF NOT EXISTS skins (id VARCHAR(16) PRIMARY KEY, name VARCHAR(16) NOT NULL, description TEXT NOT NULL, timestamp VARCHAR(16) NOT NULL)');
-db.file.setRemoteBackup(true);
-db.close();
-
-if(Utils.isiPhone()) {
-	var mainWin = Ti.UI.createWindow({
-		url: 'views/main_iphone.js',
-		tabBarHidden: true,
-		title: Ti.App.getName(),
-		backgroundImage: Utils.getBGImage(),
-		barColor: Utils.getNavColor()
-	});
-
-	var navGroup = Ti.UI.iPhone.createNavigationGroup({
-		window: mainWin
-	});
-
-	var container = Ti.UI.createWindow({
-		navBarHidden: true
-	});
-
-	container.add(navGroup);
-	container.open({
-		modal: false
-	});
-} else if(Utils.isiPad()) {
-	Ti.include('/includes/lib/json.i18n.js');
-
-	var detailWin = Ti.UI.createWindow({
-		title: I('main.skinDetails.title'),
-		barColor: Utils.getNavColor(),
-		backgroundGradient: {
-			colors: ['c6c6c6', 'e4e4e4']
+try {
+	//if the skins db exists
+	if(Ti.Filesystem.getFile(Ti.Filesystem.applicationSupportDirectory, '../Private Documents/' + Database.getDbName() + '.sql').exists()) {
+		//make a backup while it's here
+		Database.backupDatabase();
+		//continue to start the application
+		startApp();
+	} else {
+		//else, if the skins db doesn't exist
+		//check if there is a backup of it
+		if(Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, Database.getDbName() + '.sql.bck').exists()) {
+			//if there is, ask the user if he wants to restore it
+			Database.askForDatabaseRestore(function() {
+				startApp();
+			});
+		} else {
+			//if there's no backup (typically when we start the app for the first time), just start
+			startApp();
 		}
-	});
-
-	var initialInfoView = getInitialInfoView();
-	var content = initialInfoView;
-	detailWin.add(content);
-
-	var iad = Ti.UI.iOS.createAdView({
-		height: Ti.UI.SIZE,
-		width: Ti.UI.FIT,
-		bottom: 0
-	});
-
-	detailWin.add(iad);
-
-	var masterWin = Ti.UI.createWindow({
-		url: 'views/main_ipad.js',
-		backgroundImage: Utils.getBGImage(),
-		barColor: Utils.getNavColor(),
-		backgroundRepeat: true,
-		title: I('main.skinList'),
-		detailWin: detailWin,
-		detailContent: content,
-		initialInfoView: initialInfoView
-	});
-
-	var splitWin = Ti.UI.iPad.createSplitWindow({
-		detailView: Ti.UI.iPhone.createNavigationGroup({
-			window: detailWin
-		}),
-		masterView: Ti.UI.iPhone.createNavigationGroup({
-			window: masterWin
-		}),
-		showMasterInPortrait: true
-	});
-
-	splitWin.open();
-
-	masterWin.masterGroup = splitWin.masterView;
-	masterWin.detailGroup = splitWin.detailView;
-
-	detailWin.masterGroup = splitWin.masterView;
-	detailWin.detailGroup = splitWin.detailView;
-
-	function getInitialInfoView() {
-		var view = Ti.UI.createView({
-			height: Ti.UI.SIZE,
-			width: Ti.UI.FILL,
-			layout: 'vertical'
-		});
-
-		var lbl_noskin = Ti.UI.createLabel({
-			text: I('main.noSelectedSkin'),
-			width: Ti.UI.FILL,
-			textAlign: 'center',
-			left: view.size.width / 2 - this.width,
-			height: Ti.UI.SIZE,
-			font: {
-				fontSize: 30
-			},
-			color: 'gray'
-		});
-
-		view.add(lbl_noskin);
-
-		var b_add = Ti.UI.createButton({
-			title: I('main.addSkin'),
-			font: {
-				fontSize: 23
-			},
-			backgroundImage: '/img/button.png',
-			width: Ti.UI.FILL,
-			height: 50,
-			left: '8%',
-			right: '8%',
-			top: 40
-		});
-
-		b_add.addEventListener('click', function() {
-			var info_win = Ti.UI.createWindow({
-				url: 'views/add_process/info.js',
-				title: I('addProcess.skinInfo.title'),
-				backgroundImage: Utils.getBGImage(),
-				barColor: Utils.getNavColor(),
-				backgroundRepeat: true,
-				masterGroup: splitWin.masterView
-			});
-			
-			info_win.addEventListener('close', function(e) {
-				b_add.setEnabled(true);
-			});
-			
-			b_add.setEnabled(false);
-			splitWin.masterView.open(info_win);
-		});
-
-		view.add(b_add);
-
-		var b_settings = Ti.UI.createButton({
-			title: I('main.editSettings'),
-			font: {
-				fontSize: 23
-			},
-			backgroundImage: '/img/button.png',
-			width: Ti.UI.FILL,
-			height: 50,
-			left: '8%',
-			right: '8%',
-			top: 10
-		});
-
-		b_settings.addEventListener('click', function() {
-			var win_settings = Ti.UI.createWindow({
-				title: I('settings.title'),
-				backgroundImage: Utils.getBGImage(),
-				backgroundRepeat: true,
-				barColor: Utils.getNavColor(),
-				url: 'views/settings.js',
-				masterGroup: splitWin.masterView
-			});
-			
-			win_settings.addEventListener('close', function(e) {
-				b_settings.setEnabled(true);
-			});
-			
-			b_settings.setEnabled(false);
-			splitWin.masterView.open(win_settings);
-		});
-
-		view.add(b_settings);
-
-		return view;
 	}
+} catch(e) {
+	//if we catch any error, check if there's a backup and ask the user if he wants to restore it
+	if(Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, Database.getDbName() + '.sql.bck').exists()) {
+		Database.askForDatabaseRestore(function() {
+			startApp();
+		});
+	} else {
+		//if there's nothing we can do, just act like nothing happened
+		startApp();
+	}
+}
 
+function startApp() {
+	Database.initializeDatabase();
+
+	if(Utils.isiPhone()) {
+		var mainWin = Ti.UI.createWindow({
+			url: 'views/main_iphone.js',
+			tabBarHidden: true,
+			title: Ti.App.getName(),
+			backgroundImage: Utils.getBGImage(),
+			barColor: Utils.getNavColor()
+		});
+
+		var navGroup = Ti.UI.iPhone.createNavigationGroup({
+			window: mainWin
+		});
+
+		var container = Ti.UI.createWindow({
+			navBarHidden: true
+		});
+
+		container.add(navGroup);
+		container.open({
+			modal: false
+		});
+	} else if(Utils.isiPad()) {
+		Ti.include('/includes/lib/json.i18n.js');
+
+		var detailWin = Ti.UI.createWindow({
+			title: I('main.skinDetails.title'),
+			barColor: Utils.getNavColor(),
+			backgroundGradient: {
+				colors: ['c6c6c6', 'e4e4e4']
+			}
+		});
+
+		var initialInfoView = getInitialInfoView();
+		var content = initialInfoView;
+		detailWin.add(content);
+
+		var iad = Ti.UI.iOS.createAdView({
+			height: Ti.UI.SIZE,
+			width: Ti.UI.FIT,
+			bottom: 0
+		});
+
+		detailWin.add(iad);
+
+		var masterWin = Ti.UI.createWindow({
+			url: 'views/main_ipad.js',
+			backgroundImage: Utils.getBGImage(),
+			barColor: Utils.getNavColor(),
+			backgroundRepeat: true,
+			title: I('main.skinList'),
+			detailWin: detailWin,
+			detailContent: content,
+			initialInfoView: initialInfoView
+		});
+
+		var splitWin = Ti.UI.iPad.createSplitWindow({
+			detailView: Ti.UI.iPhone.createNavigationGroup({
+				window: detailWin
+			}),
+			masterView: Ti.UI.iPhone.createNavigationGroup({
+				window: masterWin
+			}),
+			showMasterInPortrait: true
+		});
+
+		splitWin.open();
+
+		masterWin.masterGroup = splitWin.masterView;
+		masterWin.detailGroup = splitWin.detailView;
+
+		detailWin.masterGroup = splitWin.masterView;
+		detailWin.detailGroup = splitWin.detailView;
+
+		function getInitialInfoView() {
+			var view = Ti.UI.createView({
+				height: Ti.UI.SIZE,
+				width: Ti.UI.FILL,
+				layout: 'vertical'
+			});
+
+			var lbl_noskin = Ti.UI.createLabel({
+				text: I('main.noSelectedSkin'),
+				width: Ti.UI.FILL,
+				textAlign: 'center',
+				left: view.size.width / 2 - this.width,
+				height: Ti.UI.SIZE,
+				font: {
+					fontSize: 30
+				},
+				color: 'gray'
+			});
+
+			view.add(lbl_noskin);
+
+			var b_add = Ti.UI.createButton({
+				title: I('main.addSkin'),
+				font: {
+					fontSize: 23
+				},
+				backgroundImage: '/img/button.png',
+				width: Ti.UI.FILL,
+				height: 50,
+				left: '8%',
+				right: '8%',
+				top: 40
+			});
+
+			b_add.addEventListener('click', function() {
+				var info_win = Ti.UI.createWindow({
+					url: 'views/add_process/info.js',
+					title: I('addProcess.skinInfo.title'),
+					backgroundImage: Utils.getBGImage(),
+					barColor: Utils.getNavColor(),
+					backgroundRepeat: true,
+					masterGroup: splitWin.masterView
+				});
+
+				info_win.addEventListener('close', function(e) {
+					b_add.setEnabled(true);
+				});
+
+				b_add.setEnabled(false);
+				splitWin.masterView.open(info_win);
+			});
+
+			view.add(b_add);
+
+			var b_settings = Ti.UI.createButton({
+				title: I('main.editSettings'),
+				font: {
+					fontSize: 23
+				},
+				backgroundImage: '/img/button.png',
+				width: Ti.UI.FILL,
+				height: 50,
+				left: '8%',
+				right: '8%',
+				top: 10
+			});
+
+			b_settings.addEventListener('click', function() {
+				var win_settings = Ti.UI.createWindow({
+					title: I('settings.title'),
+					backgroundImage: Utils.getBGImage(),
+					backgroundRepeat: true,
+					barColor: Utils.getNavColor(),
+					url: 'views/settings.js',
+					masterGroup: splitWin.masterView
+				});
+
+				win_settings.addEventListener('close', function(e) {
+					b_settings.setEnabled(true);
+				});
+
+				b_settings.setEnabled(false);
+				splitWin.masterView.open(win_settings);
+			});
+
+			view.add(b_settings);
+
+			return view;
+		}
+
+	}
 }
--- Resources/includes/db.js
+++ Resources/includes/db.js
@@ -5,7 +5,7 @@
 
 	exports.getSkins = function() {
 		var rows = [];
-		var db = Ti.Database.open('skins');
+		var db = Ti.Database.open(exports.getDbName());
 		var skinList = db.execute('SELECT * FROM skins ORDER BY name');
 
 		while(skinList.isValidRow()) {
@@ -42,10 +42,10 @@
 			} else if(Utils.isiPad()) {
 				row.hasChild = true;
 			}
-		
+
 			var img_skin = Ti.UI.createImageView({
 				image: Ti.Filesystem.getFile(Utils.getSkinsDir() + skinList.fieldByName('id') + '/front.png').read().imageAsCropped({
-					height: 42, 
+					height: 42,
 					width: 42,
 					x: 21,
 					y: 0
@@ -72,10 +72,69 @@
 
 	exports.getSkinCount = function() {
 		var rows = [];
-		var db = Ti.Database.open('skins');
+		var db = Ti.Database.open(exports.getDbName());
 		var rowCount = db.execute('SELECT * FROM skins').getRowCount();
 		db.close();
 		return rowCount;
 	}
 
+	exports.getDbName = function() {
+		return 'skins';
+	}
+
+	exports.backupDatabase = function() {
+		try {
+			var db = Ti.Filesystem.getFile(Ti.Filesystem.applicationSupportDirectory, '../Private Documents/' + exports.getDbName() + '.sql');
+			var backup = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, exports.getDbName() + '.sql.bck');
+
+			if(backup != null) {
+				backup.deleteFile();
+			}
+
+			backup.write(db.read());
+		} catch(e) {
+			Ti.API.info('Error making a backup of the database: ' + e);
+		}
+	}
+
+	exports.restoreDatabase = function() {
+		try {
+			var db = Ti.Filesystem.getFile(Ti.Filesystem.applicationSupportDirectory, '../Private Documents/' + exports.getDbName() + '.sql');
+			var backup = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, exports.getDbName() + '.sql.bck');
+
+			if(backup != null) {
+				db.deleteFile();
+				db.write(backup.read());
+			}
+		} catch(e) {
+			Ti.API.info('Error restoring a backup of the database: ' + e);
+		}
+	}
+
+	exports.initializeDatabase = function() {
+		var db = Ti.Database.open(exports.getDbName());
+		db.execute('CREATE TABLE IF NOT EXISTS skins (id VARCHAR(16) PRIMARY KEY, name VARCHAR(16) NOT NULL, description TEXT NOT NULL, timestamp VARCHAR(16) NOT NULL)');
+		db.file.setRemoteBackup(true);
+		db.close();
+	}
+
+	exports.askForDatabaseRestore = function(successCallback) {
+		var backupTime = new Date(Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, Database.getDbName() + '.sql.bck').modificationTimestamp());
+		var alert_restore = Ti.UI.createAlertDialog({
+			title: 'Uh oh...',
+			message: "We were unable to open the file containing your skins, for some reason.\nFortunately, there's a backup from " + backupTime.toUTCString() + ". Would you like to restore it?",
+			buttonNames: ["No, thanks", "Let's do this"],
+			cancel: 0
+		});
+
+		alert_restore.addEventListener('click', function(e) {
+			if(e.index == 1) {
+				exports.restoreDatabase();
+			}
+			successCallback();
+		});
+
+		alert_restore.show();
+	}
+	
 })();
